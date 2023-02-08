f605b74eaf19e31b8de88f7e43345bd8ad3dd8fa
Salim66 <salimhasanriad8@gmail.com>
2023-02-08 09:17:39 +0600

    ref concept clear it is just listing the element v-model and update the data

---
+++ src/pages/Register.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <h1>{{ count }}</h1>
+  <input type="number" v-model="count" />
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count,
+    };
+  },
+};
+</script>
