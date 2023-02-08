0b92acae69d686e9a6efffe84b97376604a60cd0
Salim66 <salimhasanriad8@gmail.com>
2023-02-08 14:37:40 +0600

    Deleting User

--- src/pages/users/Users.vue
+++ src/pages/users/Users.vue
@@ -16,7 +16,16 @@
           <td>{{ user.first_name }} {{ user.last_name }}</td>
           <td>{{ user.email }}</td>
           <td>{{ user.role.name }}</td>
-          <td></td>
+          <td>
+            <div class="btn-group mr-2">
+              <a
+                href="#"
+                class="btn btn-sm btn-outline-secondary"
+                @click.prevent="del(user.id)"
+                >Delete</a
+              >
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -36,6 +45,7 @@
 <script lang="ts">
 import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
+import { User } from '@/models/user';
 export default {
   name: 'Users',
   setup() {
@@ -65,10 +75,19 @@ export default {
       }
     };
 
+    const del = async (id: number) => {
+      if (confirm('Are you sure?')) {
+        await axios.delete(`users/${id}`);
+
+        users.value = users.value.filter((u: User) => u.id !== id);
+      }
+    };
+
     return {
       users,
       prev,
       next,
+      del,
     };
   },
 };
